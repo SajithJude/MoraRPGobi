614ebcbbe93bad8d0c40c42a47fe77ad734fa4cf
Jude Sajith <judesajith.aj@gmail.com>
2023-07-07 20:20:36 +0530

    ssss

--- pages/agent.py
+++ pages/agent.py
@@ -7,9 +7,21 @@ from llama_index import VectorStoreIndex, SimpleDirectoryReader, ServiceContext
 from langchain.llms import OpenAI
 
 
-from llama_index.prompts.system import SHAKESPEARE_WRITING_ASSISTANT
+from llama_index import Prompt
+context_str = """(1) You ask me a question about the text
 
+(2) I respond to your question
 
+(3) You give me feedback on my response.
+"""
+template = (
+    "We have provided context information below. \n"
+    "---------------------\n"
+    "{context_str}"
+    "\n---------------------\n"
+    "Given this information, please answer the question: {query_str}\n"
+)
+custom_prompt = Prompt(template)
 
 butt = st.button("load")
 
@@ -21,7 +33,7 @@ if butt:
     data = SimpleDirectoryReader(input_dir="data").load_data()
     index = VectorStoreIndex.from_documents(data, service_context=service_context)
     chat_engine = index.as_chat_engine(
-        service_context=service_context, chat_mode="react",system_prompt=SHAKESPEARE_WRITING_ASSISTANT, verbose=True
+        service_context=service_context, chat_mode="react",text_qa_template=custom_prompt, verbose=True
     )
     st.session_state.chat_engine = chat_engine
     st.success("success")
@@ -30,5 +42,5 @@ ques = st.text_input("Question")
 ask = st.button("ask")
 
 if ask:
-    response = st.session_state.chat_engine.chat(ques)
+    response = st.session_state.chat_engine.chat(f"Use the tool to answer:{ques}")
     st.write(response)
