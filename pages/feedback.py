ad6f1957fa55e0acc8a8dc5a7061cbf3f0d046a8
Jude Sajith <judesajith.aj@gmail.com>
2023-07-08 10:09:51 +0530

    sss

--- pages/feedback.py
+++ pages/feedback.py
@@ -223,14 +223,13 @@ st.title("AI Tutor")
 
 uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
 if uploaded_file is not None:
-
-    if "index" not in st.session_state:
-        st.session_state.index = process_pdf(uploaded_file)
-    res  = st.session_state.index.query("Please list 10 keywords or topics from the document").response
-    keywords = res.split('\n')
-        # st.success("Index created successfully")
-    # keywords = tutor.extract_keywords(text)
-    st.session_state.keywords = keywords
+    # if "index" not in st.session_state:
+    st.session_state.index = process_pdf(uploaded_file)
+res  = st.session_state.index.query("Please list 10 keywords or topics from the document").response
+keywords = res.split('\n')
+    # st.success("Index created successfully")
+# keywords = tutor.extract_keywords(text)
+st.session_state.keywords = keywords
 
 
 if "selected_keywords" not in st.session_state:
