ef58ffced6a1e31e944359b099c125901be8d1c2
Jude Sajith <judesajith.aj@gmail.com>
2023-07-08 07:21:26 +0530

    ssss

--- pages/feedback.py
+++ pages/feedback.py
@@ -115,7 +115,7 @@ class TutorAgent:
 
     def generate_question_answer(self, keyword: str) -> (str, str):
         self.reset()
-        message = self._llm.chat([ChatMessage(role="system", content=f"Generate a question about the topic: {keyword} and also provide the expected answer.")])
+        message = self._llm.chat([ChatMessage(role="system", content=f"Generate a question about the topic: {keyword} with the answer.")])
         question, expected_answer = message.message.content.split('\n')  # Assuming the model returns question and answer separated by a newline
         self.expected_answer = expected_answer
         return question, expected_answer
