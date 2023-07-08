a7a3c744322976285c573160c8cfd66f1de56152
Jude Sajith <judesajith.aj@gmail.com>
2023-07-08 11:56:59 +0530

    sss

--- pages/feedback.py
+++ pages/feedback.py
@@ -228,18 +228,23 @@ uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
 if uploaded_file is not None:
     # if "index" not in st.session_state:
     st.session_state.index = process_pdf(uploaded_file)
+
+
+
+if st.button()  
     res  = st.session_state.index.query("Please list 10 keywords or topics from the document").response
     keywords = res.split('\n')
         # st.success("Index created successfully")
     # keywords = tutor.extract_keywords(text)
-    keywords = keywords
+    if "keywords" not in st.session_state:
+        st.session_state.keywords = keywords
 
 
 
 # if "selected_keywords" not in st.session_state:
 #     selected_keywords
 
-selected_keywords = st.multiselect('Select topics for questions', keywords, default= keywords[1])
+selected_keywords = st.multiselect('Select topics for questions', st.session_state.keywords, default= st.session_state.keywords[1])
 
 if st.button("Start learning Session"):
     current_keyword = selected_keywords.pop(0)
