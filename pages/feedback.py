a2cda12d010ceaca3bf3344e2468e6c0537f9ff1
Jude Sajith <judesajith.aj@gmail.com>
2023-07-09 16:25:50 +0530

    sss

--- pages/feedback.py
+++ pages/feedback.py
@@ -200,8 +200,17 @@ class TutorAgent:
         return feedback, score
 
     def get_score(self, user_answer: str, expected_answer: str) -> float:
-        return sentence_bleu([expected_answer.split()], user_answer.split())
-
+        
+        Prompt = """
+        evaluate on a general understanding of how well the user's answer aligns with the expected answer.
+        In a simple format, I could use a scale of 1-10 where 1 signifies no alignment with the expected answer and 10 signifies perfect alignment.
+        Return only a number between 1 and 10 in the response.
+        Expected answer: {expected_answer}
+        User's answer: {user_answer}
+        """
+        message = self._llm.chat([ChatMessage(role="system", content=f"Generate a question about the topic: {keyword} with the answer separated by a newline.")])
+        score = message.message.content
+        return score
 
 
 # try:
